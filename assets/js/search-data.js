ced085f6017c7af0101a15c3993e3b1c27d19b58
x1chenn <xichen0522@gmail.com>
2026-05-16 19:02:55 +0000

    Deploying to gh-pages from @ x1chenn/x1chenn.github.io@245e9b17e478af125c632e312cae9bfd753afc03 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -458,13 +458,6 @@ ninja.data = [{
           window.open("https://github.com/x1chenn", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
