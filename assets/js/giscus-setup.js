9e14590757f9a496ceda9228cc9cbdf863197f3e
x1chenn <xichen0522@gmail.com>
2026-05-16 05:35:27 +0000

    Deploying to gh-pages from @ x1chenn/xichenn.github.io@de6cfde0a28d994128029064017a608cec7ea0a7 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "alshedivat/al-folio",
+    "data-repo": "x1chenn/xichenn.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
